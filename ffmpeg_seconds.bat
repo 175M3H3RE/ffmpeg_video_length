2f18c4087a2c92606f3b7c9c831e317e48b455e2
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-02-06 00:52:53 +0530

    Add files via upload

--- ffmpeg_seconds.bat
+++ ffmpeg_seconds.bat
@@ -1,10 +1,13 @@
 @echo off
 if "%1" NEQ "chutiya" start cmd /c ""%~fp0" chutiya"& goto :eof
+set /a counter=0
+:funky_loooper
+title fUNkY raBBiT (C} slEASY Soul .
 echo *
 echo * ffmpeg video length calculator (time format hh:mm to seconds)
 echo *-------------------------------------------------------------
 echo [[[[]     Usage: Enter time in MM:SS or HH:MM:SS           []]]]
-echo.Press 1 to Save, Press S to Replay, C CLear
+echo.Press 1 to Save, Press S to Replay, C CLear X Copy
 :looper
 set /a hour=0
 set /a minute=0
@@ -24,7 +27,10 @@ if "%tokens_found%"=="" goto skip3
 if "%tokens_found%" NEQ "%nonsense_bydefault%" (echo %tokens_found%|findstr /r "^[0-9]*$" >NUL&&(goto process3) || (echo.Invalid Third Char Found&goto looper) )
 
 :skip3
-for /f "tokens=1,2 delims=:" %%i in ("%str%") do set /a hour=%%i&set /a minute=%%j
+title ffmpeg video length calc.
+echo %str%|findstr /r "^[0-9:.]*$">NUL&&echo.>NUL || (echo.Non-number value&goto looper)
+set stumped=%%j
+for /f "tokens=1,2 delims=:" %%i in ("%str%") do set /a hour=%%i&if "x%%jx" NEQ "xx" set /a minute=%%j
 echo %hour%|findstr /r "^[0-9]*$">NUL&&echo.>NUL || (echo.Non-number value&goto looper)
 echo %minute%|findstr /r "^[0-9]*$">NUL&&echo.>NUL || (echo.Non-number value&goto looper)
 set /a hour=hour*60
@@ -33,6 +39,7 @@ if defined args if "%args%"=="/s" (echo %total% >>%savetofile%)
 echo %total% seconds
 goto :save
 :process3
+title ffmpeg video length calc.
 for /f "tokens=1,2,3 delims=:" %%i in ("%str%") do set /a hour=%%i&set /a minute=%%j&set /a seconds=%%k
 set /a hour=hour*60*60
 set /a minute=minute*60
@@ -45,10 +52,15 @@ if defined args if "%args%"=="/s" (echo.X&echo %total% >>%savetofile%)
 echo %total% seconds
 :save
 echo|set/p=Save 
-choice /c y1SCabdefghijklmnopqrtuvwxz023456789 /N /m "?                                                 >" /t 4 /d 0
+choice /c y1SCxhabdefgijklmnopqrtuvwz023456789 /N /m "?                                                 _Y/1/Z/0" /t 4 /d 0
+set /a boom=0
 if %errorlevel%==1 (echo..-X-&echo %total% >>%savetofile%&title Saved to file %savetofile%) 
 if %errorlevel%==2 (echo..-X-x-X-.&echo %total% >>%savetofile%&title Saved to file %savetofile%) 
-if %errorlevel%==3 if exist  %savetofile% type %savetofile%
+if %errorlevel%==3 if exist  %savetofile% type %savetofile%&set /a boom=1
 if %errorlevel%==4 (echo.Clearing file in 30 seconds.&echo.Press any key to clear immediately&echo.Close Window to avoid..&pause >NUL&timeout 30 &del %savetofile%)
-
+if %errorlevel%==5 echo|set/p=....  To Clipboard&echo|set/p=%total%|clip&set /a boom=1&echo.
+if %errorlevel%==6 set /a boom=1
+if %boom%==1 echo -Help Menu ~ (H)  .Show this menu.  ---^| Y/1 = Save ^| S = Show ^| C Clean File ^| X to Clipb ^|-
+set /a counter+=1
+if %counter% GTR 5 CLS&set /a counter=0&goto funky_loooper
 goto looper
