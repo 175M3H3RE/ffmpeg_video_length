bdfc6ac0ba6e31aeeb39209b90ee6d66459d0813
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-02-07 18:20:28 +0530

    Add files via upload

--- ffmpeg_simpleX.bat
+++ ffmpeg_simpleX.bat
@@ -7,29 +7,105 @@ echo *
 echo * ffmpeg video length calculator (time format hh:mm to seconds)
 echo *-------------------------------------------------------------
 echo [[[[]     Usage: Enter time in MM:SS or HH:MM:SS           []]]]
-echo.Press 1/S Save, (H) Help
+echo.secret command: youtube
 :looper
 set /a hour=0
 set /a minute=0
 set /a seconds=0
+if defined watchis for /f "tokens=*" %%i in ("https://www.youtube.com/watch?v=%watchis%&t=%total%") do echo %%i
+set str=
 echo|set /p=^^^>
 set /p str=
+set watchis=
+set /a come_here=0
+call :seterrorlevel989
+if %come_here%==1 goto next_X
+goto afterseterrorlevel
+:seterrorlevel989
+Exit /B 989  
+:afterseterrorlevel
+if %errorlevel%==9009 set str="%str%"
+echo %str%| findstr /r "["\"]"&&echo."FOUND erroneous character...."&&(for /f "tokens=1 delims=&" %%i in (%str%) do set str=%%i)
+
+if %errorlevel%==9009 set str="%str%"
+echo %str%| findstr /r "["\"]"&&echo."FOUND erroneous character...." || (if "%str%"==""  goto looper)
+if %errorlevel%==989 echo.check quote&goto looper
+if %errorlevel%==9009 echo.check invalid quoting&goto looper
+if "%str%"==""  goto looper
+if /I "%str%"=="cls"  (cls&goto funky_loooper)
+if /I "%str%" NEQ "youtube"  (goto continue)
+:enterurl
+set /p youtube_url=Enter Url 
+set /a come_here=1
+CALL :seterrorlevel989
+:next_X
+echo %youtube_url%
+if %errorlevel%==9009 set youtube_url="%youtube_Url%"
+echo %youtube_url% | findstr /r "["\"]"&&for /f "tokens=1 delims=&" %%i in (%youtube_url%) do set youtube_url=%%i
+echo %youtube_url% | findstr /r "["\"]"&&for /f "tokens=*" %%i in (%youtube_url%) do set youtube_url=%%i
+if %errorlevel%==989 echo.check quote&goto enterurl
+if %errorlevel%==9009 echo.RAN ERROR due to ampersand in Url..Never mind&goto enterurl
+echo %youtube_url% | find "&"&&(for /f "tokens=1 delims=&" %%i in ("%youtube_url%") do set youtube_url="%%i") || (for /f "tokens=*" %%i in ("%youtube_url%") do set youtube_url="%%i")
+if %errorlevel%==989 echo.check quote&goto enterurl
+echo %youtube_url%
+set youtube_url=%youtube_url: =%
+for /f "tokens=*" %%i in (%youtube_url%) do set youtube_url=%%i
+
+set /a pass=0
+:youtube
+
+
+set /a tokens=5
+(echo %youtube_url%|findstr /r "^https://youtu.be/") >NUL&&(goto processyoutu.be)
+if %errorlevel%==9009 goto nowell
+(echo %youtube_url%|findstr /r "^http://youtu.be/") >NUL&&(goto processyoutu.be)
+if %errorlevel%==9009 goto nowell
+(echo %youtube_url%|findstr /r "^https://youtube.com/") >NUL&&(goto processyoutube)
+if %errorlevel%==9009 goto nowell
+(echo %youtube_url%|findstr /r "^http://youtube.com/") >NUL&&(goto processyoutube)
+if %errorlevel%==9009 goto nowell
+(echo %youtube_url%|findstr /r "^https://www.youtube.com/") >NUL&&(goto wwwprocessyoutube)
+if %errorlevel%==9009 goto nowell
+(echo %youtube_url%|findstr /r "^http://www.youtube.com/") >NUL&&(goto wwwprocessyoutube)
+if %errorlevel%==9009 goto nowell
+goto continue
+:nowell
+for /f "tokens=*" %%i in (%youtube_url%) do set youtube_url="%%i"
+for /f "tokens=1 delims=&" %%i in (%youtube_url%) do set youtube_url="%%i"
+for /f "tokens=*" %%i in (%youtube_url%) do set youtube_url=%%i
+goto youtube
+:processyoutu.be
+set /a tokens=4
+for /f "tokens=%tokens% delims=.:?/" %%i in ("%youtube_url%") do set watchis=%%i
+goto continue
+:wwwprocessyoutube
+
+set /a tokens=7
+for /f "tokens=%tokens% delims=^=.:?/" %%i in ("%youtube_url%") do set watchis=%%i
+goto continue
+:processyoutube
+
+set /a tokens=6
+for /f "tokens=%tokens% delims=^=.:?/" %%i in ("%youtube_url%") do set watchis=%%i
+goto continue
+:continue
+if defined watchis set str=&set /p str=Enter video time ^>
 set nonsense_bydefault=%%i
 @echo off
 rem set args=%2
 rem set str=%1
 set str=%str:.=:%
 set str=%str: =%
+
 set savetofile=ffmpeg.time.list.txt
 set tokens_found=
 for /f "tokens=3 delims=:" %%i in ("%str%") do set tokens_found=%%i
-
 if "%tokens_found%"=="" goto skip3
 if "%tokens_found%" NEQ "%nonsense_bydefault%" (echo %tokens_found%|findstr /r "^[0-9]*$" >NUL&&(goto process3) || (echo.Invalid Third Char Found&goto looper) )
 
 :skip3
 title ffmpeg video length calc.
-echo %str%|findstr /r "^[0-9:.]*$">NUL&&echo.>NUL || (echo.Non-number&timeout 5 >NUL&cls&goto funky_loooper)
+echo %str%|findstr /r "^[0-9:.]*$">NUL&&echo.>NUL || (echo.Non-number or invalid command&timeout 5 >NUL&cls&goto funky_loooper)
 set stumped=%%j
 for /f "tokens=1,2 delims=:" %%i in ("%str%") do set /a hour=%%i&if "x%%jx" NEQ "xx" set /a minute=%%j
 echo %hour%|findstr /r "^[0-9]*$">NUL&&echo.>NUL || (echo.Non-number&goto looper)
